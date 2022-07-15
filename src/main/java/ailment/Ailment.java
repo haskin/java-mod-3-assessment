057a9bcd696709d95db864eb1de1392942639397
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-15 13:07:54 -0400

    Added ailment index

--- src/main/java/ailment/Ailment.java
+++ src/main/java/ailment/Ailment.java
@@ -3,13 +3,20 @@ package ailment;
 import util.Specialty;
 
 public enum Ailment {
-    MOLE_REMOVAL(Specialty.DERMATOLOGY, true),
-    CANCER(Specialty.RADIOLOGY, true),
-    COLD(Specialty.PEDIATRICS, true),
+    MOLE_REMOVAL(Specialty.DERMATOLOGY, 90, true),
+    CANCER(Specialty.RADIOLOGY, 10, true),
+    COLD(Specialty.PEDIATRICS, 60, true),
     DIABITIES(Specialty.DERMATOLOGY, false);
 
     private Specialty specialty;
+    private Integer ailmentIndex;
     private boolean curable;
+
+    private Ailment(Specialty specialty, Integer ailmentIndex, boolean curable) {
+        this.specialty = specialty;
+        this.ailmentIndex = ailmentIndex;
+        this.curable = curable;
+    }
 
     private Ailment(Specialty specialty, boolean curable) {
         this.specialty = specialty;
